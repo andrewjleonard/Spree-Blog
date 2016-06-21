f4c08d3ac228c8f2d0d83bb7124ece63b4eea6d6
flinchtheone <professional@aleonard.co.uk>
2016-06-21 21:39:45 +0100

    Added meta basic

--- app/controllers/spree/posts_controller.rb
+++ app/controllers/spree/posts_controller.rb
@@ -1,11 +1,28 @@
 module Spree
   class PostsController < Spree::StoreController
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
 
     def index
-    	@posts = Spree::Post.all
+      @posts = Spree::Post.all
+      @title = "Blog"
+      def my_meta_data
+        meta = ({
+                  description: "Find out all the latest news including useful hints and tips including how clean and care for your handbag.",
+        })
+      end
     end
     def show
-    	@post = Spree::Post.friendly.find(params[:id])
+      @post = Spree::Post.friendly.find(params[:id])
+      @title = @post.title
+      def my_meta_data
+        meta = ({
+                  description: @post.meta_description
+        })
+      end
     end
-end
+
+
+
+  end
 end
